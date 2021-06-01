ebfa29dc48ea8924305a78c40054d73b5c6e6166
alexfilatov2000 <tanyaarni@gmail.com>
2021-06-01 17:55:17 +0300

    graph added

--- stattrack/app/src/main/java/com/stattrack/ChartsActivity.kt
+++ stattrack/app/src/main/java/com/stattrack/ChartsActivity.kt
@@ -1,13 +1,18 @@
 package com.stattrack
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import com.stattrack.R
+import androidx.appcompat.app.AppCompatActivity
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+
 
 class ChartsActivity : AppCompatActivity() {
 
     private lateinit var backButton: Button
+    private lateinit var graph: GraphView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +21,33 @@ class ChartsActivity : AppCompatActivity() {
         backButton = findViewById(R.id.back_to_main)
 
         backButton.setOnClickListener { onBackPressed() }
+
+        //Activity
+        graph = findViewById(R.id.graph)
+        val series: LineGraphSeries<DataPoint> = LineGraphSeries(
+            arrayOf(
+                DataPoint(0.0, 1.1),
+                DataPoint(1.0, 5.0),
+                DataPoint(2.0, 3.0),
+                DataPoint(3.0, 2.0),
+                DataPoint(4.0, 6.0)
+
+            )
+        )
+        graph.addSeries(series)
+
+        //Temperature
+        graph = findViewById(R.id.graph2)
+        val series2: LineGraphSeries<DataPoint> = LineGraphSeries(
+            arrayOf(
+                DataPoint(0.0, -3.0),
+                DataPoint(1.0, 5.0),
+                DataPoint(2.0, 10.0),
+                DataPoint(3.0, -1.0),
+                DataPoint(4.0, 6.0)
+            )
+        )
+        graph.addSeries(series2)
+
     }
 }
